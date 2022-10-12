28699f1d478d6ec06a14737bc096c4ef651cec9c
TatheerHussain <TatheerHussain@Tatheers-MacBook-Pro.local>
2022-10-12 12:22:43 +0530

    main.py with simple get and post methods

---
+++ main.py
@@ -0,0 +1,27 @@
+#testing the setup 
+from typing import Optional, List
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app =FastAPI()
+
+
+users = []
+
+
+class User(BaseModel):
+    email: str
+    is_active:bool
+    bio: Optional[str]
+    
+    
+
+@app.get("/users", response_model= List(User))
+async def get_users():
+    return users
+
+
+@app.post("/users")
+async def create_users(user: User):
+    users.append(user)
+    return "success"
