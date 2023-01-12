17e3efe2bfc71cc5fb63ac42dbaf8161ca8810c1
TatheerHussain <TatheerHussain@Tatheers-MacBook-Pro.local>
2023-01-12 16:22:53 +0800

    loading user exepreince style

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ import user from './assets/user.svg';
 
 const form = document.querySelector('form');
 const chatContainer = docuement.querySelector('#chat_container')
+
+let loadInterbval;
+
+
+// code block to show the loading in chat box
+
+function loader(element){
+  element.textContent ='';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if (element.textContent === '....'){
+      element.textContent = '';
+    }
+  }, 300)
+}
+
+
+
+// code block for showing answer in a typing style like humans type
+//  this is just a way to imporve user experience 
+
+function typeText()
